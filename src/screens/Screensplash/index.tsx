3a57edc2303475cd6dca6b48ba09f91f41840681
Raul Lopes <raullopes@terra.com.br>
2025-09-26 09:59:40 -0400

    fix: corrigir texto na tela de splash para "Uma app by:"

--- src/screens/Screensplash/index.tsx
+++ src/screens/Screensplash/index.tsx
@@ -3,7 +3,7 @@ import { styles } from "./styles";
 export function Screensplash() {
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Uma empresa by:</Text>
+            <Text style={styles.title}>Uma app by:</Text>
             <Image 
             source={require("@assets/Logo.png")} 
             style={styles.logo}
